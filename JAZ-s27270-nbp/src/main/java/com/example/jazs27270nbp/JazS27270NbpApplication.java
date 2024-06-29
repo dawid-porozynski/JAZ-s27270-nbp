6669ba3ff413a1319944ecc201706b078ee98297
dawid-porozynski <pora13234@gmail.com>
2024-06-29 12:38:33 +0200

    test

---
+++ JAZ-s27270-nbp/src/main/java/com/example/jazs27270nbp/JazS27270NbpApplication.java
@@ -0,0 +1,13 @@
+package com.example.jazs27270nbp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JazS27270NbpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JazS27270NbpApplication.class, args);
+    }
+
+}
---
+++ JAZ-s27270-nbp/src/test/java/com/example/jazs27270nbp/JazS27270NbpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jazs27270nbp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JazS27270NbpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
